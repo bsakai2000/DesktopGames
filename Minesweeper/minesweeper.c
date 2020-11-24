ea2b8cd200f89795ba76ea3bd6e8c755fe3e8a54
Brandon Sakai <brandon.sakai@gmail.com>
2020-11-24 05:03:23 -0500

    Callback code

--- Minesweeper/minesweeper.c
+++ Minesweeper/minesweeper.c
@@ -4,10 +4,10 @@
 // Prefix for GResource files
 #define GRESOURCE_PREFIX "/com/example/DesktopGames/Minesweeper/"
 
-void print_location(GtkWidget *widget, gpointer data)
+gboolean print_location(GtkWidget* widget, GdkEvent* event, gpointer user_data)
 {
-	int* dimensions = g_object_get_data(G_OBJECT(widget), "loc");
-	printf("LEFT: %d, TOP: %d\n", dimensions[0], dimensions[1]);
+	printf("%s - LEFT: %d, TOP: %d\n", ((GdkEventButton*) event)->button==1?"Primary":"Secondary", ((int*)user_data)[0], ((int*)user_data)[1]);
+	return 0;
 }
 
 /**
@@ -43,9 +43,9 @@ int main (int argc, char *argv[])
 	GtkBuilder* main_builder = gtk_builder_new_from_resource(GRESOURCE_PREFIX "minesweeper.ui");
 
 	GtkGrid* grid = GTK_GRID(gtk_builder_get_object(main_builder, "grid"));
-	for(int i = 0; i < height; ++i)
+	for(int i = 0; i < width; ++i)
 	{
-		for(int j = 0; j < width; ++j)
+		for(int j = 0; j < height; ++j)
 		{
 			// Create new button
 			GtkButton* new_button = GTK_BUTTON(gtk_button_new());
@@ -54,17 +54,14 @@ int main (int argc, char *argv[])
 			gtk_widget_set_vexpand(GTK_WIDGET(new_button), true);
 			gtk_button_set_relief(new_button, GTK_RELIEF_NONE);
 
-			// Tell button where it is
-			int* dimensions = calloc(2, sizeof(int));
-			dimensions[0] = j;
-			dimensions[1] = i;
-			g_object_set_data(G_OBJECT(new_button), "loc", dimensions); 
-
 			// Set callback
-			g_signal_connect(new_button, "clicked", G_CALLBACK(print_location), NULL);
+			int* dimensions = calloc(2, sizeof(int));
+			dimensions[0] = i;
+			dimensions[1] = j;
+			g_signal_connect(new_button, "button-press-event", G_CALLBACK(print_location), dimensions);
 
 			// Attach button to grid
-			gtk_grid_attach(grid, GTK_WIDGET(new_button), j, i, 1, 1);
+			gtk_grid_attach(grid, GTK_WIDGET(new_button), i, j, 1, 1);
 		}
 	}
 	
